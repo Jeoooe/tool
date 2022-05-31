8e617039d3fa87f111d38825c48a0b7464ca387f
Jeoooe <60093246+Jeoooe@users.noreply.github.com>
2022-05-31 18:19:06 +0800

    可正常输入代码，'-'开头为简要指令

--- Books/TestWnd.js
+++ Books/TestWnd.js
@@ -3,6 +3,7 @@ var runBtn = undefined;
 var inputBox = undefined;
 var outputBox = undefined;
 var IS_ON = false;
+var is_first = true;    //第一行代码(感官优化)
 var TESTBOXES_INIT = () => {
     TESTBOXES = $('div.testboxes');
     runBtn = $('button.run');
@@ -20,27 +21,60 @@ var TESTBOXESSWITCH = () => {
         IS_ON = true;
     }
 }
+//从outputBox输出
+function output(out) {
+    inputBox.val('');   //清空输入框
+    if (is_first) {
+        outputBox.html(`>${out}\n`);
+        is_first = false;
+    }
+    else outputBox.html(`${outputBox.html()}>${out}\n`);
+}
+
+//指令列表
+var CMD_LIST = {
+    '-help': {
+        'func':()=>{
+            for (let key in CMD_LIST) {
+                output(`${key} : ${CMD_LIST[key].description}`);
+            }
+        },
+        'description': '输出所有指令'
+    },
+    '-showhtml':{
+        'func':() =>{
+            let html = $('html').html();
+            html = html.replace(/</g,'&lt;');
+            html = html.replace(/>/g,'&gt;');
+            output(html);
+        },
+        'description':'输出整个html'
+    }
+}
+
 
 //运行代码
-let is_first = true;
+/*
+ *      1. 输入代码执行
+ *      2. 简便指令, '-'开头,详见指令列表
+ *
+*/
 function RunAct() {
-    try {
-        let cmd = inputBox.val();
-        let out = eval(cmd);
-        if (is_first) {
-            outputBox.html(`>${out}`);
-            is_first = false;
+    let cmd = inputBox.val();
+    //判断是否是指令输入
+    if (cmd[0] === '-') {
+        if (cmd in CMD_LIST) {
+            CMD_LIST[cmd].func();
         }
-        else outputBox.html(`${outputBox.html()}\n>${out}`);
+        else output('指令错误(-help获得指令列表)');
     }
-    catch (e) {
-        if (is_first) {
-            outputBox.html(`>${e}`);
-            is_first = false;
+    else {
+        try {
+            let out = eval(cmd);
+            output(out);
         }
-        else outputBox.html(`${outputBox.html()}\n>${e}`);
-    }
-    finally {
-        inputBox.val('');//设为空值
+        catch (err) {
+            output(err);
+        }
     }
 }
